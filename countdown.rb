6edaa9a7d9e5c086ee5f56b8d85672d375ff2b2f
Mariana Garman <garman.mariana@gmail.com>
2019-03-09 17:57:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,11 @@ end
 end
 
 def countdown_with_sleep(number = 11)
-  while number >= 1
+  while number > 1
   number -= 1
   puts "#{number} SECOND(S)!"
   sleep 5
-  if number == 0
+  if number == 1
     return "HAPPY NEW YEAR!"
 end
 end
